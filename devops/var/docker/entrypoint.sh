fea2380f1b8e1d0d1cc6e4bc213bde3f2d61a246
Sudharshan <sudharshanrms2004@gmail.com>
2025-02-11 14:37:53 +0530

    Run app through supervisord

--- devops/var/docker/entrypoint.sh
+++ devops/var/docker/entrypoint.sh
@@ -9,4 +9,6 @@ if [[ "$INTERNAL_PROXY_ENABLED" != "false" ]]; then
 	cp -vf /app/supervisord_configs/caddy.conf /etc/supervisor.d/
 fi
 
+cp -vf /app/supervisord_configs/postiz.conf /etc/supervisor.d/
+
 /usr/bin/supervisord -c /etc/supervisord.conf
